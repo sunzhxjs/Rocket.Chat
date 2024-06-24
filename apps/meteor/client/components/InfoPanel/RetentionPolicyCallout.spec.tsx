c03d033ac71842fdd7f3035ef4ece8e5007eb524
gabriellsh <gabriel.henriques@rocket.chat>
2024-06-24 14:13:35 -0300

    fix usePruneWarningMessage tests

--- apps/meteor/client/components/InfoPanel/RetentionPolicyCallout.spec.tsx
+++ apps/meteor/client/components/InfoPanel/RetentionPolicyCallout.spec.tsx
@@ -14,7 +14,7 @@ describe('RetentionPolicyCallout', () => {
 		setDate();
 		const fakeRoom = createFakeRoom({ t: 'c' });
 		render(<RetentionPolicyCallout room={fakeRoom} />, { wrapper: createMock({ appliesToChannels: true, TTLChannels: 60000 }) });
-		expect(screen.getByRole('alert')).toHaveTextContent('a minute June 1, 2024, 12:30 AM');
+		expect(screen.getByRole('alert')).toHaveTextContent('a minute June 1, 2024 at 12:30 AM');
 	});
 
 	it('Should not render callout if settings are invalid', () => {
