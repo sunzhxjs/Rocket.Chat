c0981be6fb3401a1a36538f195c73419fe1e6d07
Tasso <tasso.evangelista@rocket.chat>
2024-06-24 22:35:38 -0300

    Adapt new tests

--- apps/meteor/client/hooks/usePruneWarningMessage.spec.ts
+++ apps/meteor/client/hooks/usePruneWarningMessage.spec.ts
@@ -1,10 +1,48 @@
 import type { IRoomWithRetentionPolicy } from '@rocket.chat/core-typings';
-import { renderHook } from '@testing-library/react-hooks';
+import { mockAppRoot } from '@rocket.chat/mock-providers';
+import { act, renderHook } from '@testing-library/react';
 
-import { createRenteionPolicySettingsMock as createMock } from '../../tests/mocks/client/mockRetentionPolicySettings';
 import { createFakeRoom } from '../../tests/mocks/data';
-import { setDate } from '../../tests/mocks/mockDate';
 import { usePruneWarningMessage } from './usePruneWarningMessage';
+
+const renderUsePruneWarningMessage = ({
+	room = createFakeRoom({ t: 'c' }),
+	enabled = true,
+	filesOnly = false,
+	doNotPrunePinned = false,
+	appliesToChannels = false,
+	TTLChannels = 60000,
+	appliesToGroups = false,
+	TTLGroups = 60000,
+	appliesToDMs = false,
+	TTLDMs = 60000,
+	precision = '0',
+	advancedPrecision = false,
+	advancedPrecisionCron = '*/30 * * * *',
+} = {}) =>
+	renderHook(() => usePruneWarningMessage(room), {
+		legacyRoot: true,
+		wrapper: mockAppRoot()
+			.withTranslations('en', 'core', {
+				RetentionPolicy_RoomWarning_NextRunDate: '{{maxAge}} {{nextRunDate}}',
+				RetentionPolicy_RoomWarning_Unpinned_NextRunDate: 'Unpinned {{maxAge}} {{nextRunDate}}',
+				RetentionPolicy_RoomWarning_FilesOnly_NextRunDate: 'FilesOnly {{maxAge}} {{nextRunDate}}',
+				RetentionPolicy_RoomWarning_UnpinnedFilesOnly_NextRunDate: 'UnpinnedFilesOnly {{maxAge}} {{nextRunDate}}',
+			})
+			.withSetting('RetentionPolicy_Enabled', enabled)
+			.withSetting('RetentionPolicy_FilesOnly', filesOnly)
+			.withSetting('RetentionPolicy_DoNotPrunePinned', doNotPrunePinned)
+			.withSetting('RetentionPolicy_AppliesToChannels', appliesToChannels)
+			.withSetting('RetentionPolicy_TTL_Channels', TTLChannels)
+			.withSetting('RetentionPolicy_AppliesToGroups', appliesToGroups)
+			.withSetting('RetentionPolicy_TTL_Groups', TTLGroups)
+			.withSetting('RetentionPolicy_AppliesToDMs', appliesToDMs)
+			.withSetting('RetentionPolicy_TTL_DMs', TTLDMs)
+			.withSetting('RetentionPolicy_Precision', precision)
+			.withSetting('RetentionPolicy_Advanced_Precision', advancedPrecision)
+			.withSetting('RetentionPolicy_Advanced_Precision_Cron', advancedPrecisionCron)
+			.build(),
+	});
 
 jest.useFakeTimers();
 
@@ -22,169 +60,145 @@ const getRetentionRoomProps = (props: Partial<IRoomWithRetentionPolicy['retentio
 	};
 };
 
-describe('usePruneWarningMessage hook', () => {
-	describe('Cron timer and precision', () => {
-		it('Should update the message after the nextRunDate has passaed', async () => {
-			setDate();
-			const fakeRoom = createFakeRoom({ t: 'c' });
-			const { result } = renderHook(() => usePruneWarningMessage(fakeRoom), {
-				wrapper: createMock({
-					appliesToChannels: true,
-					TTLChannels: 60000,
-				}),
-			});
-			expect(result.current).toEqual('a minute June 1, 2024, 12:30 AM');
+beforeEach(() => {
+	/*
+		It's not the same as `Date.parse('2024-06-01T00:01:00.000Z')`!
+		The date is created *at the same timezone of the node process* instead of fixed at UTC.
+	 */
+	const fakeDate = new Date(2024, 5, 1, 0, 1, 0);
+	jest.setSystemTime(fakeDate);
+});
+
+describe('Cron timer and precision', () => {
+	it('Should update the message after the nextRunDate has passaed', async () => {
+		const { result } = renderUsePruneWarningMessage({
+			appliesToChannels: true,
+			TTLChannels: 60000,
+		});
+
+		expect(result.current).toEqual('a minute June 1, 2024, 12:30 AM');
+
+		act(() => {
 			jest.advanceTimersByTime(31 * 60 * 1000);
-			expect(result.current).toEqual('a minute June 1, 2024, 1:00 AM');
 		});
 
-		it('Should return the default warning with precision set to every_hour', () => {
-			const fakeRoom = createFakeRoom({ t: 'c' });
-			setDate();
-			const { result } = renderHook(() => usePruneWarningMessage(fakeRoom), {
-				wrapper: createMock({
-					appliesToChannels: true,
-					TTLChannels: 60000,
-					precision: '1',
-				}),
-			});
-			expect(result.current).toEqual('a minute June 1, 2024, 1:00 AM');
-		});
-
-		it('Should return the default warning with precision set to every_six_hours', () => {
-			const fakeRoom = createFakeRoom({ t: 'c' });
-			setDate();
-			const { result } = renderHook(() => usePruneWarningMessage(fakeRoom), {
-				wrapper: createMock({
-					appliesToChannels: true,
-					TTLChannels: 60000,
-					precision: '2',
-				}),
-			});
-			expect(result.current).toEqual('a minute June 1, 2024, 6:00 AM');
-		});
-
-		it('Should return the default warning with precision set to every_day', () => {
-			const fakeRoom = createFakeRoom({ t: 'c' });
-			setDate();
-			const { result } = renderHook(() => usePruneWarningMessage(fakeRoom), {
-				wrapper: createMock({
-					appliesToChannels: true,
-					TTLChannels: 60000,
-					precision: '3',
-				}),
-			});
-			expect(result.current).toEqual('a minute June 2, 2024, 12:00 AM');
-		});
-
-		it('Should return the default warning with advanced precision', () => {
-			const fakeRoom = createFakeRoom({ t: 'c' });
-			setDate();
-			const { result } = renderHook(() => usePruneWarningMessage(fakeRoom), {
-				wrapper: createMock({
-					appliesToChannels: true,
-					TTLChannels: 60000,
-					advancedPrecision: true,
-					advancedPrecisionCron: '0 0 1 */1 *',
-				}),
-			});
-			expect(result.current).toEqual('a minute July 1, 2024, 12:00 AM');
-		});
+		expect(result.current).toEqual('a minute June 1, 2024, 1:00 AM');
 	});
 
-	describe('No override', () => {
-		it('Should return the default warning', () => {
-			const fakeRoom = createFakeRoom({ t: 'c' });
-			setDate();
-			const { result } = renderHook(() => usePruneWarningMessage(fakeRoom), {
-				wrapper: createMock({
-					appliesToChannels: true,
-					TTLChannels: 60000,
-				}),
-			});
-			expect(result.current).toEqual('a minute June 1, 2024, 12:30 AM');
+	it('Should return the default warning with precision set to every_hour', () => {
+		const { result } = renderUsePruneWarningMessage({
+			appliesToChannels: true,
+			TTLChannels: 60000,
+			precision: '1',
 		});
 
-		it('Should return the unpinned messages warning', () => {
-			const fakeRoom = createFakeRoom({ t: 'c' });
-			setDate();
-			const { result } = renderHook(() => usePruneWarningMessage(fakeRoom), {
-				wrapper: createMock({
-					appliesToChannels: true,
-					TTLChannels: 60000,
-					doNotPrunePinned: true,
-				}),
-			});
-			expect(result.current).toEqual('Unpinned a minute June 1, 2024, 12:30 AM');
-		});
-
-		it('Should return the files only warning', () => {
-			const fakeRoom = createFakeRoom({ t: 'c' });
-			setDate();
-
-			const { result } = renderHook(() => usePruneWarningMessage(fakeRoom), {
-				wrapper: createMock({
-					appliesToChannels: true,
-					TTLChannels: 60000,
-					filesOnly: true,
-				}),
-			});
-			expect(result.current).toEqual('FilesOnly a minute June 1, 2024, 12:30 AM');
-		});
-
-		it('Should return the unpinned files only warning', () => {
-			const fakeRoom = createFakeRoom({ t: 'c' });
-			setDate();
-
-			const { result } = renderHook(() => usePruneWarningMessage(fakeRoom), {
-				wrapper: createMock({
-					appliesToChannels: true,
-					TTLChannels: 60000,
-					filesOnly: true,
-					doNotPrunePinned: true,
-				}),
-			});
-			expect(result.current).toEqual('UnpinnedFilesOnly a minute June 1, 2024, 12:30 AM');
-		});
+		expect(result.current).toEqual('a minute June 1, 2024, 1:00 AM');
 	});
 
-	describe('Overriden', () => {
-		it('Should return the default warning', () => {
-			const fakeRoom = createFakeRoom({ t: 'p', ...getRetentionRoomProps() });
-			setDate();
-			const { result } = renderHook(() => usePruneWarningMessage(fakeRoom), {
-				wrapper: createMock(),
-			});
-			expect(result.current).toEqual('30 days June 1, 2024, 12:30 AM');
+	it('Should return the default warning with precision set to every_six_hours', () => {
+		const { result } = renderUsePruneWarningMessage({
+			appliesToChannels: true,
+			TTLChannels: 60000,
+			precision: '2',
 		});
 
-		it('Should return the unpinned messages warning', () => {
-			const fakeRoom = createFakeRoom({ t: 'p', ...getRetentionRoomProps({ excludePinned: true }) });
-			setDate();
-			const { result } = renderHook(() => usePruneWarningMessage(fakeRoom), {
-				wrapper: createMock(),
-			});
-			expect(result.current).toEqual('Unpinned 30 days June 1, 2024, 12:30 AM');
+		expect(result.current).toEqual('a minute June 1, 2024, 6:00 AM');
+	});
+
+	it('Should return the default warning with precision set to every_day', () => {
+		const { result } = renderUsePruneWarningMessage({
+			appliesToChannels: true,
+			TTLChannels: 60000,
+			precision: '3',
 		});
 
-		it('Should return the files only warning', () => {
-			const fakeRoom = createFakeRoom({ t: 'p', ...getRetentionRoomProps({ filesOnly: true }) });
-			setDate();
+		expect(result.current).toEqual('a minute June 2, 2024, 12:00 AM');
+	});
 
-			const { result } = renderHook(() => usePruneWarningMessage(fakeRoom), {
-				wrapper: createMock(),
-			});
-			expect(result.current).toEqual('FilesOnly 30 days June 1, 2024, 12:30 AM');
+	it('Should return the default warning with advanced precision', () => {
+		const { result } = renderUsePruneWarningMessage({
+			appliesToChannels: true,
+			TTLChannels: 60000,
+			advancedPrecision: true,
+			advancedPrecisionCron: '0 0 1 */1 *',
 		});
 
-		it('Should return the unpinned files only warning', () => {
-			const fakeRoom = createFakeRoom({ t: 'p', ...getRetentionRoomProps({ excludePinned: true, filesOnly: true }) });
-			setDate();
+		expect(result.current).toEqual('a minute July 1, 2024, 12:00 AM');
+	});
+});
 
-			const { result } = renderHook(() => usePruneWarningMessage(fakeRoom), {
-				wrapper: createMock(),
-			});
-			expect(result.current).toEqual('UnpinnedFilesOnly 30 days June 1, 2024, 12:30 AM');
+describe('No override', () => {
+	it('Should return the default warning', () => {
+		const { result } = renderUsePruneWarningMessage({
+			appliesToChannels: true,
+			TTLChannels: 60000,
 		});
+
+		expect(result.current).toEqual('a minute June 1, 2024, 12:30 AM');
+	});
+
+	it('Should return the unpinned messages warning', () => {
+		const { result } = renderUsePruneWarningMessage({
+			appliesToChannels: true,
+			TTLChannels: 60000,
+			doNotPrunePinned: true,
+		});
+
+		expect(result.current).toEqual('Unpinned a minute June 1, 2024, 12:30 AM');
+	});
+
+	it('Should return the files only warning', () => {
+		const { result } = renderUsePruneWarningMessage({
+			appliesToChannels: true,
+			TTLChannels: 60000,
+			filesOnly: true,
+		});
+
+		expect(result.current).toEqual('FilesOnly a minute June 1, 2024, 12:30 AM');
+	});
+
+	it('Should return the unpinned files only warning', () => {
+		const { result } = renderUsePruneWarningMessage({
+			appliesToChannels: true,
+			TTLChannels: 60000,
+			filesOnly: true,
+			doNotPrunePinned: true,
+		});
+
+		expect(result.current).toEqual('UnpinnedFilesOnly a minute June 1, 2024, 12:30 AM');
+	});
+});
+
+describe('Overriden', () => {
+	it('Should return the default warning', () => {
+		const { result } = renderUsePruneWarningMessage({
+			room: createFakeRoom({ t: 'p', ...getRetentionRoomProps() }),
+		});
+
+		expect(result.current).toEqual('30 days June 1, 2024, 12:30 AM');
+	});
+
+	it('Should return the unpinned messages warning', () => {
+		const { result } = renderUsePruneWarningMessage({
+			room: createFakeRoom({ t: 'p', ...getRetentionRoomProps({ excludePinned: true }) }),
+		});
+
+		expect(result.current).toEqual('Unpinned 30 days June 1, 2024, 12:30 AM');
+	});
+
+	it('Should return the files only warning', () => {
+		const { result } = renderUsePruneWarningMessage({
+			room: createFakeRoom({ t: 'p', ...getRetentionRoomProps({ filesOnly: true }) }),
+		});
+
+		expect(result.current).toEqual('FilesOnly 30 days June 1, 2024, 12:30 AM');
+	});
+
+	it('Should return the unpinned files only warning', () => {
+		const { result } = renderUsePruneWarningMessage({
+			room: createFakeRoom({ t: 'p', ...getRetentionRoomProps({ excludePinned: true, filesOnly: true }) }),
+		});
+
+		expect(result.current).toEqual('UnpinnedFilesOnly 30 days June 1, 2024, 12:30 AM');
 	});
 });
