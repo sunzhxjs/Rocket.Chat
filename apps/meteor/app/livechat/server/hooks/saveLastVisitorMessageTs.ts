ed9f0be6257005b3807dd686039aa6507b1f95ee
Ricardo Garim <rswarovsky@gmail.com>
2024-07-25 00:21:26 -0300

    chore: remove afterSaveMessage save-last-visitor-message-timestamp

--- apps/meteor/app/livechat/server/hooks/saveLastVisitorMessageTs.ts
+++ apps/meteor/app/livechat/server/hooks/saveLastVisitorMessageTs.ts
@@ -1,22 +1,9 @@
-import { isOmnichannelRoom } from '@rocket.chat/core-typings';
-import { LivechatRooms } from '@rocket.chat/models';
-
 import { callbacks } from '../../../../lib/callbacks';
 
 callbacks.add(
 	'afterSaveMessage',
-	async (message, room) => {
-		if (!(isOmnichannelRoom(room) && room.v.token)) {
-			return message;
-		}
-		if (message.t) {
-			return message;
-		}
-		if (!message.token) {
-			return message;
-		}
-
-		await LivechatRooms.setVisitorLastMessageTimestampByRoomId(room._id, message.ts);
+	async (message) => {
+		return message;
 	},
 	callbacks.priority.HIGH,
 	'save-last-visitor-message-timestamp',
